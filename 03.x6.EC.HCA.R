4688a3ba300516965cb32832b09219cd01698759
Qiongzi Qiu <qqiu@wentletrap.hpc.arizona.edu>
2025-04-06 22:25:02 -0700

    Auto backup 2025-04-06 22:25:02

--- 03.x6.EC.HCA.R
+++ 03.x6.EC.HCA.R
@@ -2,10 +2,11 @@ library(Seurat)
 library(SeuratDisk)
 library(hdf5r)
 library(sceasy)
+library(dplyr)
 
 
+### overall estimate
 seurat = sceasy::convertFormat("/xdisk/mliang1/qqiu/data/HeartCellAtlas/visium-OCT_LV_lognormalised.updated.h5ad", from = "anndata", to = "seurat")
-
 
 FeaturePlot(seurat, "IL1R1", reduction = "spatial", split.by = "sangerID")
 FeaturePlot(seurat, "EC6_ven", reduction = "spatial", split.by = "sangerID")
@@ -19,7 +20,6 @@ FeaturePlot(seurat, "KCNT2", reduction = "spatial", split.by = "sangerID")
 FeaturePlot(seurat, "LRRC3B", reduction = "spatial", split.by = "sangerID")
 FeaturePlot(seurat, "HMCN1", reduction = "spatial", split.by = "sangerID")
 FeaturePlot(seurat, "UNC5C", reduction = "spatial", split.by = "sangerID")
-
 
 
 ec_list = colnames(seurat@meta.data)[grepl("EC", colnames(seurat@meta.data))]
@@ -77,7 +77,6 @@ ggplot(cor_matrix_melted, aes(Var2, Var1, fill = value)) +
 
 
 # library(lsa)
-
 combined <- cbind(expr_matrix, cell_matrix)
 combined <- cbind(expr_matrix, ec_matrix)
 
@@ -101,10 +100,198 @@ ggplot(cor_matrix_melted, aes(Var2, Var1, fill = value)) +
 
 
 
+### cell type specific signature and moran's i
+library(spdep)
+
+r2h = read.table("/xdisk/mliang1/qqiu/reference/biomaRt/biomaRt.gene.rat2human.out.txt", header = T, sep = "\t")
+m2h = read.table("/xdisk/mliang1/qqiu/reference/biomaRt/biomaRt.gene.mouse2human.out.txt", header = T, sep = "\t")
+r2h_ortho_1 = data.frame(gene_rodent = c(r2h[r2h$Human.orthology.confidence..0.low..1.high.==1, ]$Gene.stable.ID,
+                                         r2h[r2h$Human.orthology.confidence..0.low..1.high.==1, ]$Gene.name,
+                                         m2h[m2h$Human.orthology.confidence..0.low..1.high.==1, ]$Gene.stable.ID,
+                                         m2h[m2h$Human.orthology.confidence..0.low..1.high.==1, ]$Gene.name),
+                         gene_human = c(r2h[r2h$Human.orthology.confidence..0.low..1.high.==1, ]$Human.gene.name,
+                                        r2h[r2h$Human.orthology.confidence..0.low..1.high.==1, ]$Human.gene.name,
+                                        m2h[m2h$Human.orthology.confidence..0.low..1.high.==1, ]$Human.gene.name,
+                                        m2h[m2h$Human.orthology.confidence..0.low..1.high.==1, ]$Human.gene.name))
+r2h_ortho_1 = unique(r2h_ortho_1[rowSums(is.na(r2h_ortho_1))==0, ])
+
+r2h_ortho_all = data.frame(gene_rodent = c(r2h[r2h$Human.gene.name!="", ]$Gene.stable.ID,
+                                         r2h[r2h$Human.gene.name!="", ]$Gene.name,
+                                         m2h[m2h$Human.gene.name!="", ]$Gene.stable.ID,
+                                         m2h[m2h$Human.gene.name!="", ]$Gene.name),
+                         gene_human = c(r2h[r2h$Human.gene.name!="", ]$Human.gene.name,
+                                        r2h[r2h$Human.gene.name!="", ]$Human.gene.name,
+                                        m2h[m2h$Human.gene.name!="", ]$Human.gene.name,
+                                        m2h[m2h$Human.gene.name!="", ]$Human.gene.name))
+r2h_ortho_all = unique(r2h_ortho_all[rowSums(is.na(r2h_ortho_all))==0, ])
+
+
+deg_list = read.table("/xdisk/mliang1/qqiu/project/multiomics-hypertension/cross-organ_EC/DEG/ec.scvi.gene_nb.hvg_1k.refined.merged.DEG_all.out", header = T, sep = " ")
+deg_list = deg_list[deg_list$p_val_adj<0.05 & abs(deg_list$avg_log2FC)>0.25, ] %>%
+  group_by(cell_type) %>% arrange(desc(pct.diff)) %>% ungroup()
+
+common_ec_list = c("Pecam1", "Egfl7")
+lv_ec_list = c("Nav3", "Ablim3", "Ccdc85a", "Ncald", "Nrp1", "Kitlg", "Dach1", "Arhgap18", "Ank3")
+
+top_n = 10
+m0610_marker_list = c(deg_list[deg_list$cell_type=="M0610", ]$gene_name[1:top_n], common_ec_list, lv_ec_list)
+m0610_marker_human = unique(r2h_ortho_all[r2h_ortho_all$gene_rodent %in% m0610_marker_list, ]$gene_human)
+
+m24_marker_list = c(deg_list[deg_list$cell_type=="M24", ]$gene_name[1:top_n], common_ec_list, lv_ec_list)
+m24_marker_human = unique(r2h_ortho_all[r2h_ortho_all$gene_rodent %in% m24_marker_list, ]$gene_human)
+
+m5813_marker_list = c(deg_list[deg_list$cell_type=="M5813", ]$gene_name[1:top_n], common_ec_list, lv_ec_list)
+m5813_marker_human = unique(r2h_ortho_all[r2h_ortho_all$gene_rodent %in% m5813_marker_list, ]$gene_human)
+
+c7_marker_list = c(deg_list[deg_list$cell_type=="C7", ]$gene_name[1:top_n], common_ec_list, lv_ec_list)
+c7_marker_human = unique(r2h_ortho_all[r2h_ortho_all$gene_rodent %in% c7_marker_list, ]$gene_human)
+
+marker_list = list("M0610" = m0610_marker_human,
+                   "M24" = m24_marker_human,
+                   "M5813" = m5813_marker_human,
+                   "C7" = c7_marker_human)
+
+
+m0610_marker_list = c("Hdac9", "Nrp2", "Lamc1", "Myo10")
+m0610_marker_human = unique(r2h_ortho_all[r2h_ortho_all$gene_rodent %in% m0610_marker_list, ]$gene_human)
+
+m24_marker_list = c("Hdac9", "Nrp2", "Lamc1", "Ano4")
+m24_marker_human = unique(r2h_ortho_all[r2h_ortho_all$gene_rodent %in% m24_marker_list, ]$gene_human)
+
+m5813_marker_list = c("Hdac9", "Fhod3", "Unc5c", "Il1r1", "Hmcn1", "Vwf")
+m5813_marker_human = unique(r2h_ortho_all[r2h_ortho_all$gene_rodent %in% m5813_marker_list, ]$gene_human)
+
+c7_marker_list = c("Myo10", "Lrrc3b", "Btnl9", "Kcnt2", "Nebl", "St6galnac3")
+c7_marker_human = unique(r2h_ortho_all[r2h_ortho_all$gene_rodent %in% c7_marker_list, ]$gene_human)
+
+marker_list = list("M0610" = m0610_marker_human,
+                   "M24" = m24_marker_human,
+                   "M5813" = m5813_marker_human,
+                   "C7" = c7_marker_human)
+
+# seurat = AddModuleScore(seurat, features = marker_list, name = names(marker_list), slot = "data")
+
+slide_ids <- unique(seurat$sangerID)
+signature_list = paste0(names(marker_list), c(1, 2, 3, 4))
+
+seurat@meta.data[, signature_list] <- 0
+for (slide in slide_ids) {
+  slide_cells <- WhichCells(seurat, expression = sangerID == slide)
+  slide_obj <- subset(seurat, cells = slide_cells)
+  
+  slide_obj <- AddModuleScore(
+    slide_obj,
+    features = marker_list,
+    name = names(marker_list),
+    slot = "data"
+  )
+  
+  # Store back or collect scores
+  seurat@meta.data[slide_cells, signature_list] <- slide_obj@meta.data[, signature_list]
+}
+
+seurat_meta = seurat@meta.data
+
+
+FeaturePlot(seurat, "M06101", reduction = "spatial", split.by = "sangerID")
+FeaturePlot(seurat, "M242", reduction = "spatial", split.by = "sangerID")
+FeaturePlot(seurat, "M58133", reduction = "spatial", split.by = "sangerID")
+FeaturePlot(seurat, "C74", reduction = "spatial", split.by = "sangerID")
+
+
+
+all_results <- list()
+signature_list = paste0(names(marker_list), c(1, 2, 3, 4))
+signature_list = c("EC5_art", "EC6_ven", "EC7_endocardial")
+
+for (slide_id in unique(seurat_meta$sangerID)) {
+  slide_results <- list()  # Store results for all signatures for this slide
+  
+  coords <- seurat_meta[seurat_meta$sangerID == slide_id, c("array_row", "array_col")]
+  
+  knn <- knearneigh(coords, k = 6)
+  nb <- knn2nb(knn)
+  listw <- nb2listw(nb, style = "W")
+  
+  for (signature_id in signature_list) {
+    
+    # Extract signature score for this slide and signature
+    gene_expr <- seurat_meta[seurat_meta$sangerID == slide_id, signature_id]
+    
+    # Check for enough non-NA values
+    if (sum(!is.na(gene_expr)) >= 3) {  # Moran’s I requires at least 3 non-NA values
+      moran_i <- moran.test(gene_expr, listw)
+      slide_results[[signature_id]] <- moran_i$estimate[["Moran I statistic"]]
+    } else {
+      slide_results[[signature_id]] <- NA
+    }
+  }
+  
+  all_results[[slide_id]] <- slide_results
+}
+# Combine into a data frame
+moran_df <- do.call(cbind, all_results)  # rows = genes, cols = slides
+moran_df
+
+
+
+
+all_results <- list()
+gene_list = c("IL1R1", "SMAD6")
+
+for (slide_id in unique(seurat_meta$sangerID)) {
+  slide_results <- list()  # Store results for all signatures for this slide
+  
+  coords <- seurat_meta[seurat_meta$sangerID == slide_id, c("array_row", "array_col")]
+  
+  knn <- knearneigh(coords, k = 6)
+  nb <- knn2nb(knn)
+  listw <- nb2listw(nb, style = "W")
+  
+  for (gene in gene_list) {
+    
+    # Extract signature score for this slide and signature
+    gene_expr <- seurat@assays$RNA$data[gene, seurat_meta$sangerID == slide_id]
+    
+    # Check for enough non-NA values
+    if (sum(!is.na(gene_expr)) >= 3) {  # Moran’s I requires at least 3 non-NA values
+      moran_i <- moran.test(gene_expr, listw)
+      slide_results[[gene]] <- moran_i$estimate[["Moran I statistic"]]
+    } else {
+      slide_results[[gene]] <- NA
+    }
+  }
+  
+  all_results[[slide_id]] <- slide_results
+}
+# Combine into a data frame
+moran_df <- do.call(cbind, all_results)  # rows = genes, cols = slides
+moran_df
 
 
 
 
 
+moran_long <- as.data.frame(moran_df) %>%
+  tibble::rownames_to_column("signature") %>%
+  tidyr::pivot_longer(
+    cols = -signature,
+    names_to = "sample",
+    values_to = "moran_i"
+  ) %>%
+  mutate(moran_i = as.numeric(moran_i))
 
+ggplot(moran_long, aes(x = signature, y = moran_i)) +
+  geom_jitter(width = 0.2, height = 0, size = 2, alpha = 0.7) +
+  stat_summary(fun = mean, geom = "point", color = "red", size = 3, shape = 18) +  # optional mean dot
+  labs(
+    x = "Signature",
+    y = "Moran's I",
+    title = "Moran's I per EC Signature"
+  ) +
+  theme_classic() +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1, colour = "black"),
+    plot.title = element_text(hjust = 0.5)
+  )
 
